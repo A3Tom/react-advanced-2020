fe367da3d6fc879a1c37170de048024fcb4a6483
A3Tom <Tom_Esq@Yahoo.com>
2021-01-07 21:37:09 +0000

    Completed useEffect secition of tutorial

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+	const [value, setValue] = useState(0);
+
+	useEffect(() => {
+		console.log("call useEffect");
+
+		if (value >= 1) {
+			document.title = `New Messages(${value})`;
+		}
+	}, [value]);
+
+	useEffect(() => {
+		// This only runs once, can have multiple of the same useEffect method ... Javascript is off the fuckin rails man
+		console.log("Hello world");
+	}, []);
+
+	console.log("render component");
+
+	return (
+		<>
+			<h1>{value}</h1>
+			<button
+				className="btn"
+				onClick={() => {
+					setValue(value + 1);
+				}}
+			>
+				hings
+			</button>
+		</>
+	);
 };
 
 export default UseEffectBasics;
