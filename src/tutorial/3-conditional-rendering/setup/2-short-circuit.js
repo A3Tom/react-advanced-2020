07ca126061bc205b5c04314e015e405f57f7ae81
A3Tom <Tom_Esq@Yahoo.com>
2021-01-07 23:59:26 +0000

    Completed conditional formatting

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+	const [text, setText] = useState("");
+	const [isError, setIsError] = useState(false);
 
-  return <h2>short circuit</h2>;
+	const firstValue = text || "hello world";
+	const secondValue = text && "hello world";
+
+	return (
+		<>
+			<h1>{firstValue}</h1>
+			<h1>value : {secondValue}</h1>
+
+			<h1>{text || "Remo"}</h1>
+			{text && <h1>Hello World</h1>}
+			{!text && <h1>Hello World</h1>}
+
+			<button className="btn" onClick={() => setIsError(!isError)}>
+				Clickity click, motha fucka
+			</button>
+			{isError && <h1>Error ...</h1>}
+			{isError ? (
+				<p>There's an error mate</p>
+			) : (
+				<div>
+					<p>Everything is sound</p>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default ShortCircuit;
